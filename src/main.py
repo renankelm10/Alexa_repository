2b1d3ffb40b7ae38e153d7b934e418849ede83a0
Renan Kelm <77139050+renankelm10@users.noreply.github.com>
2025-09-05 14:25:19 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from flask import Flask, send_from_directory
 
-from src.routes.user import user_bp
-from src.routes.alexa import alexa_bp
+
+
 
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
 app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
