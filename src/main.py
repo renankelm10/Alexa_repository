fdab3cdadd7c48544ed44e5a675b1c8b2fb98b36
Renan Kelm <77139050+renankelm10@users.noreply.github.com>
2025-09-05 14:14:54 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from flask import Flask, send_from_directory
-from src.models.user import db
+
 from src.routes.user import user_bp
 from src.routes.alexa import alexa_bp
 
